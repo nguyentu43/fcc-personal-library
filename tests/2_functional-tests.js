5e537f95178025ee125ca2f1ae8ffc5e5dbe885e
Glitch (abalone-circle) <none>
2019-06-23 08:47:12 +0000

    🛬🚊 Checkpoint ./tests/2_functional-tests.js:5024683/1647

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -72,7 +72,14 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          done();
+        });
       });      
       
     });
@@ -81,11 +88,25 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books/' + '5d0f37081e2ef93519a45e40')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'no book exists');
+          done();
+        });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books/' + '5d0f39b97b581d44e3738d3a')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.title, 'Node.js');
+          done();
+        });
       });
       
     });
@@ -94,7 +115,18 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai
+        .request(server)
+        .post('/api/books/' + '5d0f39b97b581d44e3738d3a')
+        .send({comment: 'assert'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.deepInclude(res.body, {
+            title: 'Node.js',
+            comments: ['assert']
+          });
+          done();
+        });
       });
       
     });
